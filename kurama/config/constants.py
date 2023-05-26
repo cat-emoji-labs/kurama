28dd5b42bff76e2a83ec55ead42c903d2d0ce389
alec <fkalec@gmail.com>
2023-05-26 16:48:34 -0700

    add DEFAULT_AST_ALLOWED_TYPES

--- kurama/config/constants.py
+++ kurama/config/constants.py
@@ -1,1 +1,18 @@
+import ast
+
 OPENAI_MODEL = "gpt-3.5-turbo"
+
+DEFAULT_AST_ALLOWED_TYPES = (
+    ast.Module,
+    ast.Expr,
+    ast.Dict,
+    ast.Str,
+    ast.Attribute,
+    ast.Num,
+    ast.Name,
+    ast.Load,
+    ast.Tuple,
+    ast.List,
+    ast.UnaryOp,  # for descending/ascending in a pipeline
+    ast.USub,  # for descending/ascending in a pipeline
+)
